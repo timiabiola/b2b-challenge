7a7bb2167a633e1012d242b1185401f53de1f341
timiabiola <timi@enlightenedinformatics.com>
2025-10-06 01:08:56 -0600

    Center blueprint button on all screen sizes

--- src/components/hero/HeroSection.tsx
+++ src/components/hero/HeroSection.tsx
@@ -49,7 +49,7 @@ export function HeroSection() {
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.6, delay: 0.4 }}
-              className="flex justify-center lg:justify-start"
+              className="flex justify-center"
             >
               <Button
                 size="default"
